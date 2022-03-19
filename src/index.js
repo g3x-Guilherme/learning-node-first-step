fb1930658a35aa46317d34a35ba79ebfedcc9477
g3xnegative1 <pedrog3x7@gmail.com>
2022-03-18 23:46:26 -0300

    query paramns

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ const { URL } = require('url');
 const routes = require('./routes');
 
 const server = http.createServer((request, response) =>  {
-  const parsedUrl = new URL('http://localhost:3000/users?order=desc');
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
 
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter((Boolean));
   
-
-  console.log(`Resquest method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+  
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    routeObj.endpoint === pathname && routeObj.method === request.method
     ));
 
     if (route) {
       request.query = Object.fromEntries(parsedUrl.searchParams)
+      request.params = { id };
+
+      response.send = (statusCode, body) => {
+        response.writeHead(400, { 'Content-type': 'application/json' });
+        response.end(JSON.stringify({error: 'User not found'}));
+      }
+
       route.handler(request, response);
     } else {
       response.writeHead(404, { 'Content-type': 'text/html' });
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
 const UserController = require("./controllers/UserController")
 
 module.exports = [
+  
   {
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: UserController.getUserById,
+  },
+];
+
+/**{
     endpoint: '/users',
     method: 'GET',
     handler: UserController.listUsers,
-  },
-  {
-    endpoint: '/products',
-    method: 'GET',
-    handler: UserController.listUsers,
-  },
-];
+  }, */
